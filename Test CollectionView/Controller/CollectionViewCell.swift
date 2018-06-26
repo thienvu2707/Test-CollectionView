20cf15dfbba8080547ae3acaed774c1c5d2b8e10
ThienVuLe <thienvurock@gmail.com>
2018-06-26 10:54:13 +0700

    1st commit: use collectionView and add 2 button using constraint on diff phone size

---
+++ Test CollectionView/Controller/CollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionViewCell.swift
+//  Test CollectionView
+//
+//  Created by Thien Vu Le on Jun/26/18.
+//  Copyright © 2018 Thien Vu Le. All rights reserved.
+//
+
+import Foundation
---
+++ Test CollectionView/Controller/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Test CollectionView
+//
+//  Created by Thien Vu Le on Jun/26/18.
+//  Copyright © 2018 Thien Vu Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    let reuseIdentifier = "CellIdentifier"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: CollectionView Delegate and datasource
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        
+        return cell
+    }
+}
+
